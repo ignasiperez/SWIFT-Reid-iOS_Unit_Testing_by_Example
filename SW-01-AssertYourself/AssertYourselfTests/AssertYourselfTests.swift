d320bb3882ff4cc707ca10c94259388665e3260d
ignasiperez <ignasiperez@gmail.com>
2022-03-09 11:17:32 +0100

    SW-01-AssertYourself -iter2

--- SW-01-AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
+++ SW-01-AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
@@ -6,82 +6,85 @@
 //
 
 import XCTest
+
 @testable import AssertYourself
+
 
 class AssertYourselfTests: XCTestCase {
 
   func test_fail() {
-    XCTFail()
+//    XCTFail()
   }
   
   
   func test_fail_withSimpleMessage() {
-    XCTFail("We have a problem")
+//    XCTFail("We have a problem")
   }
-  
-  
+
+
   func test_fail_withInterpolateMessage() {
-    let theAnswer = 42
-    
-    XCTFail("The Answer to the Great Question is \(theAnswer)")
+//    let theAnswer = 42
+//
+//    XCTFail("The Answer to the Great Question is \(theAnswer)")
   }
   
   
   func test_avoidconditionalCode() {
-    let success = false
-    if !success {
-      XCTFail()
-    }
+//    let success = false
+//
+//    if !success {
+//      XCTFail()
+//    }
   }
 
   
   func test_assertTrue() {
-    let success = false
-
-    XCTAssertTrue(success)
+//    let success = false
+//
+//    XCTAssertTrue(success)
   }
   
   
   func test_assertNil() {
-    let optionalValue: Int? = 123
-
-    XCTAssertNil(optionalValue)
+//    let optionalValue: Int? = 123
+//
+//    XCTAssertNil(optionalValue)
   }
   
   
   func test_assertNil_withSimpleStruct() {
-    let optionalValue: SimpleStruct? = SimpleStruct(x: 1, y: 2)
-    
-    XCTAssertNil(optionalValue)
+//    let optionalValue: SimpleStruct? = SimpleStruct(x: 1, y: 2)
+//
+//    XCTAssertNil(optionalValue)
   }
   
   
   func test_assertNil_withSelfDescribingType() {
-    let optionalValue: StructWithDescription =
-      StructWithDescription(x: 1, y: 2)
-    
-    XCTAssertNil(optionalValue)
+//    let optionalValue: StructWithDescription =
+//      StructWithDescription(x: 1, y: 2)
+//
+//    XCTAssertNil(optionalValue)
   }
   
   
   func test_assertEqual() {
-    let actual = "actual"
-    
-    XCTAssertEqual(actual, "expected")
+//    let actual = "hello"
+//
+//    XCTAssertEqual(actual, "hello")
   }
   
   
   func test_assertEqual_withOptional() {
-    let result: String? = "foo"
-    
-    XCTAssertEqual(result, "bar")
+//    let result: String? = "foo"
+//
+//    XCTAssertEqual(result, "bar")
   }
   
   
   func test_floatingPointDanger() {
-    let result = 0.1 + 0.2
-    
-    XCTAssertEqual(result, 0.3)
+//    let result = 0.1 + 0.2
+//
+//    XCTAssertEqual(result, 0.3)
   }
 
 
